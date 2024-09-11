548362d02b35235fad4f9063bc8fc88ec56ee63d
Саша <Alex-ST18@yandex.ru>
2024-09-11 23:21:05 +0300

    11/09 23.20

---
+++ Домашниe задания/main.py
@@ -0,0 +1,21 @@
+#"1st program"
+print(9**0.5*5)
+
+#"2st program"
+print(9.99>9.98 and 1000!=1000.1)
+
+#"3st program"
+print(2*2+2)
+print(2*(2+2))
+x = 2*2+2
+t = 2*(2+2)
+print(x==t)
+
+#"4st program"
+y = float("123.456")
+k = (y*10)
+l = (str(k)[3])
+print(int(l))
+#второй вариант решения
+t = '123.456'[4]
+print(int(t))
