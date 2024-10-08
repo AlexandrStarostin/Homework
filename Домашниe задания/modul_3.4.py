8a3f2061fa5b7b5ae50277356633ca6d098be842
Саша <Alex-ST18@yandex.ru>
2024-10-08 08:42:53 +0300

    08/10 08.42

--- Домашниe задания/modul_3.4.py
+++ Домашниe задания/modul_3.4.py
@@ -1,10 +1,11 @@
 def single_root_words(root_word, *other_words):
     same_words = []
+    # low_other_words = other_words.lower()
     for i in other_words:
-        if root_word in i:
+        if root_word.lower() in i.lower():
             same_words.append(i)
     print(same_words)
 
-single_root_words('rich', "rich", 'richyt', 'doog')
+single_root_words('rIch', "RiCh", 'richyT', 'doog')
 
 
