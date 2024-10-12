1dfbb384ef7f21148984dc4b0116d99b2c521e28
Саша <Alex-ST18@yandex.ru>
2024-10-12 22:05:14 +0300

    12/10 22.04

--- Домашниe задания/modul_3.2.py
+++ Домашниe задания/modul_3.2.py
@@ -1,3 +1,41 @@
+print('Вариант 1')
+def send_email(message, recipient, sender="university.help@gmail.com"):
+    for _ in sender:
+        if '@' in sender:
+            for j in recipient:
+                if '@' in recipient:
+                    if sender != recipient:
+                        sen = sender.endswith(('.ru', '.com', '.net'))
+                        if sen is True:
+                            rec = recipient.endswith(('.ru', '.com', '.net'))
+                            if rec is True:
+                                print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+                                break
+                            else:
+                                print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без расширения <.com>, <ru>, <net> в <recipient>')
+                                break
+                        else:
+                            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без расширения <.com>, <.ru>, <.net> в <sender>')
+                            break
+                    elif sender == recipient:
+                        print('Нельзя отправить письмо самому себе!')
+                        break
+                else:
+                    print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без <@> в <recipient>')
+                    break
+        else:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без <@> в <sender>')
+            break
+        break
+
+send_email('vbn', "university.helpg@mil.net", "university.helpg@mail.net")
+
+
+
+
+
+print('Вариант 2')
+
 def send_email(message, recipient, sender="university.help@gmail.com"):
     for _ in sender:
         if '@' in sender:
