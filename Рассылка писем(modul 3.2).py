4b52017b9625586df3c33eac7f09f38d166f193b
Саша <Alex-ST18@yandex.ru>
2024-10-12 22:50:52 +0300

    12/10 22.04

--- Рассылка писем(modul 3.2).py
+++ Рассылка писем(modul 3.2).py
@@ -1,9 +1,53 @@
+print('Вариант 1')
+def send_email( message, recipient, sender = "university.help@gmail.com"):
+    for _ in sender:
+        if '@' in sender:
+            for j in recipient:
+                if '@' in recipient:
+                    if sender != recipient:
+                        sen = sender.endswith(('.ru', '.com', '.net'))
+                        if sen is True:
+                            rec = recipient.endswith(('.ru', '.com', '.net'))
+                            if rec is True:
+                                if sender == "university.help@gmail.com":
+                                    print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+                                    break
+                                else:
+                                    print(f'Нестандартный отправитель Письмо успешно отправлено с адреса {sender} на адрес {recipient}')
+                                    break
+
+
+                            else:
+                                print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без расширения <.com>, <ru>, <net> в <recipient>')
+                                break
+                        else:
+                            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без расширения <.com>, <.ru>, <.net> в <sender>')
+                            break
+                    elif sender is recipient:
+                        print('Нельзя отправить письмо самому себе!')
+                        break
+                else:
+                    print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без <@> в <recipient>')
+                    break
+        else:
+            print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient} без <@> в <sender>')
+            break
+        break
+
+send_email('vbn', "university.helpg@mil.net", "university.helpg@mail.net")
+
+
+
+
+
+print('Вариант 2')
+
 def send_email(message, recipient, sender="university.help@gmail.com"):
     for _ in sender:
         if '@' in sender:
             for j in recipient:
                 if '@' in recipient:
-                    send = sender[::-1]  # начало после @
+                    send = sender[::-1]
                     com = send[0:4]
                     net = send[0:4]
                     ru = send[0:3]
@@ -59,4 +103,5 @@ def send_email(message, recipient, sender="university.help@gmail.com"):
             break
 
 
-send_email('vbn', "university.helpgmail.net", "university.helpg@mail.ru" )
+send_email('vbn', "university.helpg@mail.net", "university.helpg@mail.ru" )
+
