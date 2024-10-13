1980259729717a4c22bc500ecdaca3e2340dbcdd
Саша <Alex-ST18@yandex.ru>
2024-10-13 09:24:07 +0300

    13/10 09.13

--- Домашниe задания/modul_3.2.py
+++ Домашниe задания/modul_3.2.py
@@ -33,5 +33,5 @@ def send_email( message, recipient,*, sender = "university.help@gmail.com"):
             break
         break
 
-send_email('vbn', "university.helpg@mil.nt", sender= "university.help@gmail.ru")
+send_email('vbn', "university.helpg@mil.net")
 
