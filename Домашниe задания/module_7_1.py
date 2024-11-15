f6cf88dbb887c645db9cec938792f7fb4e4cd386
Саша <Alex-ST18@yandex.ru>
2024-11-15 22:11:37 +0300

    15/11 22/11

--- Домашниe задания/module_7_1.py
+++ Домашниe задания/module_7_1.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 class Product:
     def __init__(self, name, weidht, category):
         self.name = str(name)
@@ -9,11 +7,11 @@ class Product:
     def __str__(self):
         return f'{self.name}, {self.weight}, {self.category}'
 
-class Shop:
+class Shop(Product):
     __file_name = 'products.txt'
-    def __init__(self, *products):
-        # super().__init__(name, weidht, category)
-        self.products = products
+    def __init__(self,name, weidht, category):
+        super().__init__(name, weidht, category)
+
     def get_products(self):
         f = open(Shop.__file_name)
         fl = f.read()
@@ -22,29 +20,32 @@ class Shop:
 
     def add(self, *products):
         self.products = products
-        gh = self.name
         f = open(Shop.__file_name, 'r')
-        for gh in Shop.__file_name:
-            if gh in Shop.__file_name:
+        for i in Shop.__file_name:
+            i == self.name
+            if self.name in Shop.__file_name:
                 f.close()
                 return f'Продукт {self.products} уже есть в магазине'
             else:
-                f.close()
                 f = open(Shop.__file_name, 'a')
-                f.write(f'\n{self.name}')
+                f.write(f'{self.name}, {self.weight}, {self.category};\n')
                 f.close()
+                return
+
 
 
 
 p1 = Product('Potato', 50.4, 'Vegetables')
-p2 = Product('Spaghetti', 3.4, 'Groceries')
-p3 = Product('Potato', 5.5, 'Vegetables')
+p2 = Product('Spaghetti', 3.8, 'Groceries')
+p3 = Product('Carrot', 5.5, 'Vegetables')
 
-s = Shop()
+
+p4 = Shop('Pear', 50.4, 'Vegetables')
+p5 = Shop('Spaghetti', 3.4, 'Groceries')
+p6 = Shop('Appel', 5.5, 'Vegetables')
 
 print(p1)
-# print(p2)
-# print(p3)
-print(s.get_products())
 
-# print(s.add(p1, p2, p3))
+print(Shop.get_products(p5))
+
+Shop.add(p1, p4, p3, p6)
