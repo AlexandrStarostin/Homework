d9e1ab64bb1a9b3070f8b2e85c3f8d9e286104ae
Саша <Alex-ST18@yandex.ru>
2024-11-19 20:13:47 +0300

    19/11 20/13

---
+++ Домашниe задания/modul_7_1_0.py
@@ -0,0 +1,55 @@
+from itertools import product
+
+
+class Product:
+    def __init__(self, name, weight, category):
+        self.name = str(name)
+        self.weight = float(weight)
+        self.category = str(category)
+
+    def __str__(self):
+        return f'{self.name}, {self.weight}, {self.category}'
+
+class Shop:
+
+    def __init__(self):
+        self.__file_name = 'products.txt'
+
+
+    def get_products(self):
+        f = open(self.__file_name)
+        fl = f.read()
+        f.close()
+        return fl
+
+    def add(self, *products):
+        all_inf = str(self.get_products())
+        all_info = all_inf.split('\n')
+        all_info1 = str(all_info).split("' ")
+
+        file = open(self.__file_name, 'a+', encoding='UTF-8')
+        for prod in products:
+            for i in all_info:
+                pr = i.split(',')
+                if prod.name in pr:
+                    print(f"Продукт {str(prod.name)} уже есть в магазине")
+                    break
+            else:
+                file.write(str(prod) + '\n')
+                break
+
+
+        file.close()
+
+p1 = Product('Potato',50.8,'Vegetables')
+p2 = Product('Spaghetti',3.8,'Groceries')
+p3 = Product('Carrot',5.5,'Vegetables')
+#p4 = Product('ddd', 555, 'ppp')
+s = Shop()
+
+print(p1, p2, p3)
+
+s.add(p1, p2, p3)
+
+print(s.get_products())
+
