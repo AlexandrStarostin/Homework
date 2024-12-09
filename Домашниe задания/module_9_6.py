dd1014f72bcee0e7a1ce5409f5ce61d45a5fc241
Саша <Alex-ST18@yandex.ru>
2024-12-09 22:29:12 +0300

    09/12 22/28

--- Домашниe задания/module_9_6.py
+++ Домашниe задания/module_9_6.py
@@ -1,9 +1,7 @@
 def all_variants(text):
     for i in range(len(text)):
-        for j in range(i, len(text)):
-            # yield text[i:j+1]
-
-            yield text[i:(j+1)]
+        for j in range(len(text) - i):
+            yield text[j:j+i+1]
 
 a = all_variants("abc")
 
