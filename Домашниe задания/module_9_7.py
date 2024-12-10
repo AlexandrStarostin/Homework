f3fb69427001e3621eed0847a6296ab60582b4c5
Саша <Alex-ST18@yandex.ru>
2024-12-10 17:18:11 +0300

    10/12 17/18

--- Домашниe задания/module_9_7.py
+++ Домашниe задания/module_9_7.py
@@ -1,15 +1,18 @@
 def is_prime(func):
     def wrapper(*args):
-        orig_res = str(sum(args))
-        if len(orig_res) <=1:
+        orig_res = sum(args)
+        if orig_res % 2 == 1:
             print('Простое')
+            print(orig_res)
         else:
-            print(sum(args))
+            print('Составное')
+            print(orig_res)
     return wrapper
 
 @is_prime
 def sum_three(n, m, x):
     return n + m + x
 
+sum_t = sum_three(2, 4, 5)
+print(sum_t)
 
-print(sum_three(2, 6,1))
