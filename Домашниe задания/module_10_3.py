57ea36ec8e3e0c81d2a282c50635f779299f2041
Саша <Alex-ST18@yandex.ru>
2024-12-20 00:12:21 +0300

    20/12 0/12

--- Домашниe задания/module_10_3.py
+++ Домашниe задания/module_10_3.py
@@ -1,23 +1,25 @@
 import random
 import threading
 import time
+
+
 class Bank:
     check = 0
+
     def __init__(self):
         self.check = Bank.check
         self.balance = int()
         self.lock = threading.Lock()
 
     def deposit(self):
-        self.lock.acquire() # Блокировка других потоков для стабильной работы этого потока
+        # self.lock.acquire() # Блокировка других потоков для стабильной работы этого потока
         for i in range(100):
             time.sleep(0.001)
             rand = random.randint(50, 500)
             self.check += rand
             print(f'Пополнение: {rand}. Баланс: {self.check}')
-            if self.check >= 500 and self.lock.locked(): # и (проверка) заблокированы ли другие потоки
-                self.lock.release()               # Снятие блокировки других потоков
-                break
+            if self.check >= 500 and self.lock.locked():  # и (проверка) заблокированы ли другие потоки
+                self.lock.release()  # Снятие блокировки других потоков
 
     def take(self):
         for i in range(100):
@@ -29,16 +31,15 @@ class Bank:
                 time.sleep(0.001)
                 self.check -= rand
                 print(f"Снятие: {rand}, Баланс: {self.check}")
-                continue
 
             if self.check < rand:
                 self.lock.acquire()
                 time.sleep(0.001)
                 print(f"Запрос отклонён, недостаточно средств.")
-                break
 
         print()
         print(f"Итоговый баланс: {self.check}")
+
 
 bk = Bank()
 
@@ -47,7 +48,7 @@ th1 = threading.Thread(target=Bank.deposit, args=(bk,))
 th2 = threading.Thread(target=Bank.take, args=(bk,))
 
 th1.start()
-th1.join() # который не даст выполнять основную программу пока не завершится поток
+th1.join()  # который не даст выполнять основную программу пока не завершится поток
 
 th2.start()
 th2.join()
--- Домашниe задания/module_10_3_1.py
+++ Домашниe задания/module_10_3_1.py
@@ -9,18 +9,18 @@ class Bank:
         self.lock = threading.Lock()
 
     def deposit(self):
-        # self.lock.acquire() # Блокировка других потоков для стабильной работы этого потока
+        self.lock.acquire() # Блокировка других потоков для стабильной работы этого потока
         for i in range(100):
             time.sleep(0.001)
             rand = random.randint(50, 500)
             self.check += rand
             print(f'Пополнение: {rand}. Баланс: {self.check}')
-
             if self.check >= 500 and self.lock.locked(): # и (проверка) заблокированы ли другие потоки
                 self.lock.release()               # Снятие блокировки других потоков
 
 
     def take(self):
+        self.lock.acquire()
         for i in range(100):
             time.sleep(0.001)
             rand = random.randint(50, 500)
@@ -30,12 +30,14 @@ class Bank:
                 time.sleep(0.001)
                 self.check -= rand
                 print(f"Снятие: {rand}, Баланс: {self.check}")
+                # self.lock.release()
 
 
             if self.check < rand:
                 self.lock.acquire()
                 time.sleep(0.001)
                 print(f"Запрос отклонён, недостаточно средств.")
+                # self.lock.release()
 
 
         print()
@@ -48,7 +50,8 @@ th1 = threading.Thread(target=Bank.deposit, args=(bk,))
 th2 = threading.Thread(target=Bank.take, args=(bk,))
 
 th1.start()
-th1.join() # который не даст выполнять основную программу пока не завершится поток
 
 th2.start()
+
+th1.join() # который не даст выполнять основную программу пока не завершится поток
 th2.join()
