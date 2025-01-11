99793aff1c8be4ab78ed6142bc3c191c46515610
Саша <Alex-ST18@yandex.ru>
2025-01-11 17:00:52 +0300

    11/01 17/00

---
+++ Домашниe задания/module_11_3.py
@@ -0,0 +1,20 @@
+import inspect
+from pprint import pprint
+
+
+class Func:
+    def __init__(self, name):
+        self.it = 4            # класс объекта
+        sum = self.it + name
+        print(sum)
+
+tr = Func
+
+
+def introspection_info(y):
+    obj = ['Тип объекта', 'Методы и функции объекта', 'Модуль, к которому объект принадлежит',
+           'Текст кода объекта с номером строки начала кода', 'Кортеж базовых классов']
+    obj_data = [type(y), dir(y), inspect.getmodule(y), inspect.getsourcelines(y), inspect.getmro(y)]
+    pprint(dict(zip(obj, obj_data)))
+
+number_info = introspection_info(tr)
