2198d2402c7aa98166987001cb0adf4d77f9a41f
Саша <Alex-ST18@yandex.ru>
2025-01-20 21:16:16 +0300

    20/01 21/15

--- Домашниe задания/tests_12_1.py
+++ Домашниe задания/tests_12_1.py
@@ -5,57 +5,38 @@ class RunnerTest(TestCase):
 
     def test_walk(self):
         runn = Runner('Sas')
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
-        print(runn.walk())
+
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+        # print(runn.walk())
+
+        for _ in range(10):
+            print(runn.walk())
         self.assertEqual(runn.distance, 50)
 
     def test_run(self):
         ruun = Runner('Sas')
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
-        print(ruun.run())
+        for _ in (range(10)):
+            print(ruun.run())
+
         self.assertEqual(ruun.distance, 100)
 
     def test_challenge(self):
         runn_run = Runner('Dasha')
-        runn_walk = Runner('Masha')
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
-        print(runn_run.run())
+        for _ in (range(10)):
+            print(runn_run.run())
         dis_r = runn_run.distance
 
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
-        print(runn_walk.walk())
+        runn_walk = Runner('Masha')
+        for _ in (range(10)):
+            print(runn_walk.walk())
         dis_w = runn_walk.distance
 
         self.assertNotEqual(dis_r, dis_w)
